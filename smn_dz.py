740c8489959da3c1aa7155a3566f96e6532a54d7
De <spark49942@gmail.com>
2022-09-29 01:41:11 +0300

    DZ1-a

--- smn_dz.py
+++ smn_dz.py
@@ -1,43 +1,52 @@
-N = int(input('1. Введите число: '))
-num = 1
-lsd = []
-for i in range(N):
-    lsd.append(str(num))
-    num*=-3
-print(', '.join(lsd))
+# N = int(input('1. Введите число: '))
+# num = 1
+# lsd = []
+# for i in range(N):
+#     lsd.append(str(num))
+#     num*=-3
+# print(', '.join(lsd))
 
-N = int(input('2. Введите число: '))
-# print('[', end='')
-lsd = []
-for n in range(1,N+1):
-    lsd.append((3*n+1))
-# print(', '.join(lsd), end='')
-# print(']', end='')
-print(lsd)
+# N = int(input('2. Введите число: '))
+# # print('[', end='')
+# lsd = []
+# for n in range(1,N+1):
+#     lsd.append((3*n+1))
+# # print(', '.join(lsd), end='')
+# # print(']', end='')
+# print(lsd)
 
-stri_ = input('3. Введите две строки: \n')
-str_ = input()
-ar = []
-ray = []
-for i in range(len(stri_)):
-    ar.append(stri_)
-print(ar)
+# stri_ = input('3. Введите две строки: \n')
+# str_ = input()
+# ar = []
+# ray = []
+# for i in range(len(stri_)):
+#     ar.append(stri_)
+# print(ar)
 
-a = "aaaaaaaaabaasss"
-b = "aba"
-length = len(a) - len(b)
-count = 0
-for i in range(length):
-    if b in a[i:i + len(b)]:
-        count += 1
-print(count)
+# a = "aaaaaaaaabaasss"
+# b = "aba"
+# length = len(a) - len(b)
+# count = 0
+# for i in range(length):
+#     if b in a[i:i + len(b)]:
+#         count += 1
+# print(count)
 
-def f(a, b):
-    count = 0
-    for i in range(len(a) - len(b)):
-        print('->>', a[i:i + len(b)])
-        if b == a[i:i + len(b)]:
-            count += 1
-    return count
+# def f(a, b):
+#     count = 0
+#     for i in range(len(a) - len(b)):
+#         print('->>', a[i:i + len(b)])
+#         if b == a[i:i + len(b)]:
+#             count += 1
+#     return count
 
 
+dig = 0
+res = 0
+num = str(input('1. Введите число: '))
+for i in num:
+    # while dig<len(num):
+        if i.isdigit():
+            res += int(num[dig])
+        dig+=1
+print('Сумма цифр в числе', num, '=', res)
