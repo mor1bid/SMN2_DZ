a30cfded02f519a6f4127b356fa52486975c3034
De <spark49942@gmail.com>
2022-09-29 02:17:19 +0300

    DZ3-rl

--- smn_dz.py
+++ smn_dz.py
@@ -1,55 +1,32 @@
-# N = int(input('1. Введите число: '))
-# num = 1
-# lsd = []
-# for i in range(N):
-#     lsd.append(str(num))
-#     num*=-3
-# print(', '.join(lsd))
+# for i in range(4):
+from typing import List
 
-# N = int(input('2. Введите число: '))
-# # print('[', end='')
-# lsd = []
-# for n in range(1,N+1):
-#     lsd.append((3*n+1))
-# # print(', '.join(lsd), end='')
-# # print(']', end='')
-# print(lsd)
 
-# stri_ = input('3. Введите две строки: \n')
-# str_ = input()
-# ar = []
-# ray = []
-# for i in range(len(stri_)):
-#     ar.append(stri_)
-# print(ar)
+res = 0
+num = str(input('1. Введите число: '))
+for i in num:
+    if i.isdigit():
+        res += int(i)
+print('Сумма цифр в числе', num, '=', res)
 
-# a = "aaaaaaaaabaasss"
-# b = "aba"
-# length = len(a) - len(b)
-# count = 0
-# for i in range(length):
-#     if b in a[i:i + len(b)]:
-#         count += 1
-# print(count)
-
-# def f(a, b):
-#     count = 0
-#     for i in range(len(a) - len(b)):
-#         print('->>', a[i:i + len(b)])
-#         if b == a[i:i + len(b)]:
-#             count += 1
-#     return count
-
-for i in range(4):
-    res = 0
-    num = str(input('1. Введите число: '))
-    for i in num:
-        if i.isdigit():
-            res += int(i)
-    print('Сумма цифр в числе', num, '=', res)
-
+# for i in range(4):
 res = 1
 num = int(input('2. Введите число: '))
 for i in range(1, num+1):
     res *= i
     print(res)
+
+# for i in range(4):
+res = 0
+num = int(input('3. Введите число: '))
+for i in range(1, num+1):
+    res += (1+1/i)**i
+print(round(res, 3))
+
+# for i in range(4):
+print('Введите 3 числа:')
+N = int(input)
+a = int(input)
+b = int(input)
+List = []
+# for i in range(-N, N):
