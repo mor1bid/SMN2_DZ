fd18a233c6342402d643f0d75d2ce7d4eb8b1c58
De <spark49942@gmail.com>
2022-09-29 03:18:27 +0300

    DZ4-rl

--- smn_dz.py
+++ smn_dz.py
@@ -1,7 +1,4 @@
 # for i in range(4):
-from typing import List
-
-
 res = 0
 num = str(input('1. Введите число: '))
 for i in num:
@@ -12,21 +9,28 @@ print('Сумма цифр в числе', num, '=', res)
 # for i in range(4):
 res = 1
 num = int(input('2. Введите число: '))
+print('Ответ:')
 for i in range(1, num+1):
     res *= i
-    print(res)
+    print(res, '\t')
 
 # for i in range(4):
 res = 0
 num = int(input('3. Введите число: '))
 for i in range(1, num+1):
     res += (1+1/i)**i
-print(round(res, 3))
+print('Ответ:', round(res, 3))
 
 # for i in range(4):
-print('Введите 3 числа:')
-N = int(input)
-a = int(input)
-b = int(input)
+N = int(input('4. Введите значение границ списка: '))
 List = []
-# for i in range(-N, N):
+for i in range(-N, N):
+    List.append(i)
+print(List, '\n Введите желаемы значения позиций в списке: ')
+a = int(input())
+b = int(input())
+if a<N*2 and b<N*2:
+    res = List[int(a)] * List[int(b)]
+    print('Произведение элементов списка на позициях', a, 'и', b, '=', res)
+else:
+    print('Задано неверное значение позиций!') 
